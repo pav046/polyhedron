3a176d38b65eb76ff4362ed9da0b0dd075b8a0ee
pav046 <shurik-palch04@mail.ru>
2023-05-16 19:27:35 +0300

    first commit

---
+++ run_shadow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S python3 -B
+
+from time import time
+from common.tk_drawer import TkDrawer
+from shadow.polyedr import Polyedr
+
+
+tk = TkDrawer()
+try:
+    for name in ["box_simple"]:   # "ccc", "cube", "box", "king", "cow"
+        print("=============================================================")
+        print(f"Начало работы с полиэдром '{name}'")
+        start_time = time()
+        for l in range(2):
+            Polyedr(f"data/{name}.geom", l).draw(tk, l)
+        print()
+        delta_time = time() - start_time
+        print(f"Изображение полиэдра и нахождение" 
+              f" периметра проекций '{name}' заняло {delta_time} сек.")
+        input("Hit 'Return' to continue -> ")
+except(EOFError, KeyboardInterrupt):
+    print("\nStop")
+    tk.close()
---
+++ tests/test_perimetr.py
@@ -0,0 +1,20 @@
+from pytest import approx
+from shadow.polyedr import Polyedr
+
+
+class TestPerimetr:
+
+    def test_simple_1(self):
+        a = Polyedr(f"data/simple_1.geom", 1)
+        a.draw(None, 1)
+        assert  a.P == approx(0.0)
+
+    def test_simple_2(self):
+        a = Polyedr(f"data/simple_2.geom", 1)
+        a.draw(None, 1)
+        assert a.P == approx(15.03224)
+
+    def test_box_simple(self):
+        a = Polyedr(f"data/box_simple.geom", 1)
+        a.draw(None, 1)
+        assert a.P == approx(4.0)
